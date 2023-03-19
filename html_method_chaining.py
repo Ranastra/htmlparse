8bfdb0b528bc572930204407b1d76ab4bc1eda49
Ranastra <karo.schnitter@gmx.de>
2023-03-19 01:04:24 +0100

    Project class and working css

--- html_method_chaining.py
+++ html_method_chaining.py
@@ -1,31 +1,77 @@
 import copy
 from helper import get_config, format_identifiers as fm_idf
+from os import mkdir
 
 config = get_config()
 INDENT = int(config["indentation-per-level"])
 
 
 class Project():
-    def __init__(self, name:str):
-        self.HTML = HTMLFile(name)
-        self.CSS = CSSFile(name)
+    def __init__(self, path:str, name:str):
+        try:
+            mkdir(path)
+        except: pass
+        self.HTML = HTMLFile(path, name)
+        self.CSS = CSSFile(path, name)
         self.name = name
+        self.path = path
+        self.HTML.head.single_tag_child("link").set_propertys(rel="'stylesheet'", href=f"'{self.name}.css'")
+
+    def output(self):
+        self.HTML.output()
+        self.CSS.output()
+
+    def __str__(self):
+        return str(self.HTML) + "\n\n" + str(self.CSS)
+
 
 class HTMLFile():
-    def __init__(self, name:str):
+    def __init__(self, path:str, name:str):
         self.html = Tag("html")
         self.head = self.html.normal_tag_child("head")
         self.body = self.html.normal_tag_child("body")
         self.name = name
+        self.path = path
 
     def __str__(self):
         self.html.fix_indent(0)
-        return str(self.html)
+        return "<!DOCTYPE html>\n" + str(self.html)
     
+    def output(self):
+        with open(f"{self.path}/{self.name}.html", "w") as f:
+            f.write(str(self))
+    
+
 class CSSFile():
-    def __init__(self, name:str):
+    def __init__(self, path:str, name:str):
         self.name = name
-        self.rules = {"class":[], "id":[]}
+        self.path = path
+        self.__rules = {}
+
+    def new_selector(self, name:str):
+        self.__rules[name] = {}
+
+    def delet_selector(self, name:str):
+        self.__rules.pop(name, None)
+
+    def modify_selector(self, s_name, **rules):
+        self.__rules[s_name].update(rules)
+
+    def remove_rules(self, s_name, *rules):
+        for r in rules: self.__rules[s_name].pop(r, None)
+
+    def __str__(self):
+        s = "\n"
+        for sel in self.__rules:
+            s += fm_idf(sel) + " {\n"
+            for key in self.__rules[sel]:
+                s += " "*INDENT + f"{fm_idf(key)}: {self.__rules[sel][key]};\n"
+            s += "}\n\n"
+        return s
+    
+    def output(self):
+        with open(f"{self.path}/{self.name}.css", "w") as f:
+            f.write(str(self))
 
 
 class Tag():
@@ -145,7 +191,7 @@ class Tag():
     def __str__(self):
         s = " "*INDENT*self.indent_level + f"<{self.name}"
         if len(self.propertys):
-            s += " " + " ".join(f"{fm_idf(key)}={str(self.propertys[key])}" for key in self.propertys)
+            s += " " + " ".join(f"{fm_idf(key)}={fm_idf(str(self.propertys[key]))}" for key in self.propertys)
         s += ">"
         if self.content:
             s += self.content
@@ -153,7 +199,8 @@ class Tag():
             s += "\n" + "\n".join(str(c) for c in self) + "\n" + " "*INDENT*self.indent_level 
         s += f"</{self.name}>"
         return s
-    
+
+
 class STag():
     def __init__(self, name:str):
         self.name = name
@@ -214,15 +261,29 @@ class STag():
     def __str__(self):
         s = " "*INDENT*self.indent_level + f"<{self.name}"
         if len(self.propertys):
-            s += " " + " ".join(f"{fm_idf(key)}={str(self.propertys[key])}" for key in self.propertys)
+            s += " " + " ".join(f"{fm_idf(key)}={fm_idf(str(self.propertys[key]))}" for key in self.propertys)
         s += " />"
         return s
 
-f = HTMLFile("test")
-t = f.body.normal_tag_child("div").normal_tag_multi_child(["h3", "p", "p"]).duplicate()
-for i in range(2):
-    f.body[i][0].set_propertys(style="'background-color:red;'")
 
-br = STag("br")
-f.body.insert_at(1, br)
-print(f)
+# f = HTMLFile("test")
+# t = f.body.normal_tag_child("div").normal_tag_multi_child(["h3", "p", "p"]).duplicate()
+# for i in range(2):
+#     f.body[i][0].set_propertys(style="'background-color:red;'")
+
+# br = STag("br")
+# f.body.insert_at(1, br)
+# css = CSSFile("test")
+# css.new_selector("#test")
+# css.modify_selector("#test", background_color="red", color="white", font_weight="bold")
+# print(f)
+# print(css)
+p = Project("projects/test2", "test")
+p.HTML.body.normal_tag_child("div").set_propertys(Class="'important'", id="'parent_tag'").normal_tag_multi_child(["p", "p"])
+for (i, child) in enumerate(p.HTML.body[0]):
+    child.set_content("some text" + str(i)).set_propertys(Class="'child_tag'")
+p.CSS.new_selector("#parent-tag")
+p.CSS.new_selector(".child-tag")
+p.CSS.modify_selector(".child-tag", background_color="black")
+p.CSS.modify_selector("#parent-tag", font_weight="bold", color="red")
+p.output()
